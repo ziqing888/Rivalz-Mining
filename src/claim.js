4c33ca8a067a500259071a9aafca519bd57d5688
子清 <120438383+ziqing888@users.noreply.github.com>
2024-11-17 17:45:22 +0800

    Update claim.js

--- src/claim.js
+++ src/claim.js
@@ -1,16 +1,30 @@
 const moment = require('moment');
 const { delay } = require('./utils');
-const config = require('./config');
 
 async function claimFragmentz(contract, numClaims) {
   for (let i = 0; i < numClaims; i++) {
     try {
-      const tx = await contract.claim();
-      const explorerUrl = `${config.explorerUrl}/tx/${tx.hash}`;
-      console.log(`[ ${moment().format('HH:mm:ss')} ] 成功领取，交易哈希：${explorerUrl}`.green);
-      await delay(config.claimDelay || 5000);
+      const responseContract = await contract.claim();
+
+      if (responseContract.hash) {
+        console.log(
+          `[ ${moment().format('HH:mm:ss')} ] 成功为地址 ${
+            responseContract.from
+          } 领取 Fragmentz`.green
+        );
+        console.log(
+          `[ ${moment().format('HH:mm:ss')} ] 在这里查看交易哈希: https://rivalz2.explorer.caldera.xyz/tx/${
+            responseContract.hash
+          }`.green
+        );
+      }
+      await delay(5000);
     } catch (error) {
-      console.error(`[ ${moment().format('HH:mm:ss')} ] 领取失败：${error.message}`.red);
+      console.log(
+        `[ ${moment().format('HH:mm:ss')} ] 领取 Fragmentz 时出错: ${
+          error.message
+        }`.red
+      );
     }
   }
 }
