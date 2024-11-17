47780c2b7fde147640943a6786e8eb55416a28a5
子清 <120438383+ziqing888@users.noreply.github.com>
2024-11-17 15:18:04 +0800

    Create config.js

---
+++ src/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  contractAddress: '0xF0a66d18b46D4D5dd9947914ab3B2DDbdC19C2C0', // 合约地址
+  explorerUrl: 'https://rivalz2.explorer.caldera.xyz', // 区块链浏览器 URL
+  claimDelay: 5000, // 每次领取的延迟时间（单位：毫秒）
+};
